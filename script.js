18c535b5d334f284df9ac128c2c85d209fe19482
FES001 <firdavs7794948@gmail.com>
2024-04-26 14:44:59 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let numberOne = 2456;
+let numberTwo = 61541564;
+
+numbers(numberOne,numberTwo)
+
+function numbers(numberOne,numberTwo) {
+    let newResult = numberOne * numberTwo;
+    console.log(newResult);
+}
